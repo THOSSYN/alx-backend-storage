b5ce72d3f8b729104c2b38619fc7aef184832301
THOSSYN <alextosin5@gmail.com>
2024-01-23 03:43:50 +0100

    Readjusted 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     total_logs = db.nginx.count_documents({})
     print(f"{total_logs} logs")
 
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = db.nginx.count_documents({"method": method})
