2f4d3c8a2eec83f9b1836efb5732b39348d42a5e
THOSSYN <alextosin5@gmail.com>
2024-01-23 03:57:17 +0100

    Updated 12-log_stats.py to handle code failure due to tab

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = db.nginx.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        print(f"\tmethod {method}: {count}")
 
     status_check_count = db.nginx.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check_count} status check")
